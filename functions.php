61f9ac3c377319b894f8ae70d4a2260b478fff64
tenpei999 <tenpei999@gmail.com>
2022-11-20 21:35:36 +0900

    admin-bar 表示

--- functions.php
+++ functions.php
@@ -1,6 +1,23 @@
 <?php
-add_theme_support('menus');
-add_theme_support('title-tag');
+function custom_theme_support()
+{
+    add_theme_support('html5', array(
+        'menus',
+        'title-tag',
+        'search-form',
+        'gallery',
+        'caption',
+    ));
+    add_theme_support('post-thumbnails');
+    add_theme_support('title-tag');
+    add_theme_support('menus');
+    register_nav_menus(array(
+        'category_nav' => esc_html__('category navigation', 'tsuruten-web'),
+    ));
+}
+add_action('after_setup_theme', 'custom_theme_support');
+
+
 
 //タイトル出力
 function change_title($title)
@@ -15,9 +32,11 @@ function change_title($title)
 add_filter('pre_get_document_title', 'change_title');
 
 
+
+
 function  readScript()
 {
-    wp_enqueue_script('jq', get_theme_file_uri('/js/jquery-3.6.1.min.js'), array('jquery'),'', true);
+    wp_enqueue_script('jq', get_theme_file_uri('/js/jquery-3.6.1.min.js'), array('jquery'), '', true);
     wp_enqueue_script('bundle', get_theme_file_uri() . '/js/main.min.js', array('jquery'), '1.0.0', true);
     wp_enqueue_style('swiper', get_theme_file_uri() . '/swiper/css/swiper-bundle.min.css', array('cssStyle'), '1.0.0', false);
     wp_enqueue_style('Noto+Sans+JP', '//fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400;900&display=swap', array());
@@ -31,11 +50,12 @@ function  readScript()
 }
 add_action('wp_enqueue_scripts', 'readScript');
 
-function add_my_scripts() {
+function add_my_scripts()
+{
     wp_enqueue_script('swiper-js', get_theme_file_uri() . '/swiper/swiper-bundle.min.js', array('bundle'), '1.0.0', true);
     wp_enqueue_script('slider', get_theme_file_uri() . '/js/slider.min.js', array('swiper-js'), '1.0.0', true);
 }
-add_action( 'wp_enqueue_scripts', 'add_my_scripts' );
+add_action('wp_enqueue_scripts', 'add_my_scripts');
 
 //複数のメニューを登録する場合
 register_nav_menus(array(
@@ -47,7 +67,7 @@ register_nav_menus(array(
 function add_additional_class_on_li($classes, $item, $args)
 {
     if (isset($args->add_li_class)) {
-    $classes['class'] = $args->add_li_class;
+        $classes['class'] = $args->add_li_class;
     }
     return $classes;
 }
@@ -58,17 +78,15 @@ add_filter('nav_menu_css_class', 'add_additional_class_on_li', 1, 3);
 function add_additional_class_on_a($classes, $item, $args)
 {
     if (isset($args->add_li_class)) {
-    $classes['class'] = $args->add_a_class;
+        $classes['class'] = $args->add_a_class;
     }
     return $classes;
 }
 add_filter('nav_menu_link_attributes', 'add_additional_class_on_a', 1, 3);
 
-/*グローバルメニューのliのid,classを削除*/
-// add_filter('nav_menu_css_class', 'wp_navtag_remove', 100, 1); //liのclassを強制的に全削除
-// add_filter('nav_menu_item_id', 'wp_navtag_remove', 100, 1); //liのidを強制的に削除
-// add_filter('page_css_class', 'wp_navtag_remove', 100, 1); //これはテーマによって記述しなくても良い場合がありますが、念の為記述しておいてください
-// function wp_navtag_remove($var)
-// {
-//     return is_array($var) ? array_intersect($var, array('current-menu-item')) : '';
-// }
+add_filter( 'show_admin_bar', 'set_adminbar' );
+
+function set_adminbar( $adminbar ) {
+ $adminbar  = true; /* true:表示 false:非表示 */
+return $adminbar;
+}
