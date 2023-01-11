7bf01f6f09c83a837b0700770f565aac9d90e0b5
tenpei999 <tenpei999@gmail.com>
2023-01-11 12:56:14 +0900

    [feat]inokichi-dark

--- footer.php
+++ footer.php
@@ -1,8 +1,22 @@
 <footer class="p-footer">
-    <p class="p-page-top"><a href="#"><span>Page Top</span></a></p>
-    <small class="p-footer__copy c-text-small--white">
-      ©2022 tsuruten-web.com
-    </small>
+  <p
+    <?php if(is_front_page()):?>
+      class="p-page-top"
+    <?php else: ?>
+      class="p-page-top is-dark"
+    <?php endif;?>
+  >
+  <a href="#"
+  <?php if(is_front_page()):?>
+    class="is-bright"
+  <?php else: ?>
+    class="is-dark"
+  <?php endif;?>
+  >
+  <span>Page Top</span></a></p>
+  <small class="p-footer__copy c-text-small--white">
+    ©2022 tsuruten-web.com
+  </small>
   </footer>
   <?php wp_footer(); ?> 
 </body>
