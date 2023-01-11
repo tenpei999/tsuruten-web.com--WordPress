986f332da914dfaebf76911c5c4eb877795cc81c
tenpei999 <tenpei999@gmail.com>
2023-01-11 22:14:04 +0900

    [fix]投稿のマージン

--- single.php
+++ single.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-    <main class="l-main--custom-post">
+    <main class="l-main--post">
 
       <section class="p-main-visual">
 
