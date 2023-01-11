ce367f6245844e6868b9ad25e84930fd919e8652
tenpei999 <tenpei999@gmail.com>
2023-01-11 23:21:31 +0900

    [feat]page-products.php追加

---
+++ page-products.php
@@ -0,0 +1,26 @@
+<?php get_header(); ?>
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+<section class="p-main-visual">
+
+  <div style="background-image: url(
+  <?php echo wp_get_attachment_url(get_post_thumbnail_id()); ?>)" class="p-main-visual">
+
+    <h2 class="p-main-visual__title">
+      <p class="c-title--section p-main-visual__title-main">
+        <?php the_title(); ?>
+      </p>
+    </h2>
+  </div>
+</section>
+<main class="l-main--products">
+
+
+  <?php the_content(); ?>
+</main>
+
+
+<?php endwhile;
+endif; ?>
+
+<?php get_footer(); ?>
