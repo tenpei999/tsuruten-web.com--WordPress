fdf86d3d8a49490dbf3fceffd44c5117c11ec1ff
tenpei999 <tenpei999@gmail.com>
2023-01-11 22:22:20 +0900

    [fix]single.php修正

--- single.php
+++ single.php
@@ -1,26 +1,26 @@
 <?php get_header(); ?>
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+<section class="p-main-visual">
 
-    <main class="l-main--post">
+  <div style="background-image: url(
+  <?php echo wp_get_attachment_url(get_post_thumbnail_id()); ?>)" class="p-main-visual">
 
-      <section class="p-main-visual">
+    <h2 class="p-main-visual__title">
+      <p class="p-main-visual__title-sub">
+        Blog
+      </p>
+      <p class="c-title--section p-main-visual__title-main">
+        <?php the_title(); ?>
+      </p>
+    </h2>
+  </div>
+</section>
 
-        <div style="background-image: url(
-        <?php echo wp_get_attachment_url(get_post_thumbnail_id()); ?>)" class="p-main-visual">
+<main class="l-main--post">
 
-          <h2 class="p-main-visual__title">
-            <p class="p-main-visual__title-sub">
-              Blog
-            </p>
-            <p class="c-title--section p-main-visual__title-main">
-              <?php the_title(); ?>
-            </p>
-          </h2>
-        </div>
-      </section>
-
-      <?php the_content(); ?>
-    </main>
+  <?php the_content(); ?>
+  
+</main>
 
 
 <?php endwhile;
