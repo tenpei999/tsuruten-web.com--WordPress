7ac2d9c27afab13d7b52a8ba04ae673737dfd446
tenpei999 <tenpei999@gmail.com>
2023-01-12 14:44:59 +0900

    [fix]main-visual高さを縮小

--- page-products.php
+++ page-products.php
@@ -1,17 +1,14 @@
 <?php get_header(); ?>
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-<section class="p-main-visual">
+<section class="p-main-visual"style="background-image: url(
+  <?php echo wp_get_attachment_url(get_post_thumbnail_id()); ?>)">
 
-  <div style="background-image: url(
-  <?php echo wp_get_attachment_url(get_post_thumbnail_id()); ?>)" class="p-main-visual">
-
-    <h2 class="p-main-visual__title">
-      <p class="c-title--section p-main-visual__title-main">
-        <?php the_title(); ?>
-      </p>
-    </h2>
-  </div>
+  <h2 class="p-main-visual__title">
+    <p class="c-title--section p-main-visual__title-main">
+      <?php the_title(); ?>
+    </p>
+  </h2>
 </section>
 <main class="l-main--products">
 
