0a58ed9cd7a37dd1a4349117a8a70b6568731ce7
tenpei999 <tenpei999@gmail.com>
2023-01-12 22:38:56 +0900

    [feat]検索・アーカイブページ成形

--- archive.php
+++ archive.php
@@ -1,49 +1,34 @@
 <?php get_header(); ?>
-<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+<section class="p-main-visual">
+  
+  <div style="background-image: url(
+    <?php echo get_template_directory_uri(); ?>/img/tree-woods.jpg)" class="p-main-visual">
 
-<section class="p-main-visual"style="background-image: url(
-  <?php echo wp_get_attachment_url(get_post_thumbnail_id()); ?>)">
-
-  <h2 class="p-main-visual__title">
-    <p class="c-title--section p-main-visual__title-main"><?php the_archive_title(); ?></p>
-  </h2>
+<h2 class="p-main-visual__title">
+  <p class="c-title--section p-main-visual__title-main"><?php the_archive_title(); ?></p>
+</h2>
+</div>
 </section>
+<!-- main-visual-->
+
 <div class="l-wrapper p-wrapper">
   <main class="l-main--products p-main--products">
-  <?php the_post_thumbnail(); ?>
+    <article class="l-search">
+      <article>
+        <?php if (is_category()) : ?>
+          <?php echo category_description(); ?>
+        <?php endif; ?>
+      </article>
+      <?php get_template_part("components/archive"); ?>
+      
+      <?php wp_link_pages(); ?>
+    </article>
+    <!-- pages -->
 
-<section class=" c-card__contents-area">
-  <section class="c-card__text-area">
-    <h3 class="c-card__title">
-      <?php the_title(); ?>
-    </h3>
-    <?php get_single(); ?>
-    <!-- preg_match_all から取得-->
-    <?php
-
-    //小見出しh2の文字数を除外して抜粋を開始
-    $start = 6;
-
-    //取得する長さ（文字数）
-    $length = 100;
-
-    //指定した文字数を出力
-    echo mb_substr(get_the_excerpt(), $start, $length), ',,,続きを読む';
-
-    ?>
-  </section>
-  <section class="c-card__btn-area">
-    <button class="c-card__btn">
-      <a href="<?php echo get_permalink(); ?>">詳しく見る</a>
-    </button>
   </main>
   <aside class="l-sidebar p-sidebar">
     <?php get_sidebar(); ?>
   </aside>
 </div>
-
-<?php endwhile; else : ?>
-  <p>記事がありません。</p>
-<?php endif; ?>
 
 <?php get_footer(); ?>
--- search.php
+++ search.php
@@ -1,38 +1,37 @@
 <?php get_header(); ?>
-<main>
 <section class="p-main-visual">
+  
+  <div style="background-image: url(
+    <?php echo get_template_directory_uri(); ?>/img/tree-woods.jpg)" class="p-main-visual">
 
-<div style="background-image: url(
-<?php echo get_template_directory_uri(); ?>/img/tree-woods.jpg)" class="p-main-visual">
-
-  <h2 class="p-main-visual__title">
-    <p class="p-main-visual__title-sub">
-      検索結果
-    </p>
-    <p class="c-title--section p-main-visual__title-main"><?php echo get_search_query (); ?></p>
-  </h2>
+<h2 class="p-main-visual__title">
+  <p class="p-main-visual__title-sub">
+    検索結果
+  </p>
+  <p class="c-title--section p-main-visual__title-main"><?php echo get_search_query (); ?></p>
+</h2>
 </div>
 </section>
 <!-- main-visual-->
 
-  <article class="l-search">
-    <article>
-      <?php if (is_category()) : ?>
-        <?php echo category_description(); ?>
-      <?php endif; ?>
+<div class="l-wrapper p-wrapper">
+  <main class="l-main--products p-main--products">
+    <article class="l-search">
+      <article>
+        <?php if (is_category()) : ?>
+          <?php echo category_description(); ?>
+        <?php endif; ?>
+      </article>
+      <?php get_template_part("components/archive"); ?>
+      
+      <?php wp_link_pages(); ?>
     </article>
-    <?php get_template_part("components/archive"); ?>
-    
-    <?php wp_link_pages(); ?>
-  </article>
-  <!-- pages -->
+    <!-- pages -->
 
-</main>
-
+  </main>
+  <aside class="l-sidebar p-sidebar">
+    <?php get_sidebar(); ?>
+  </aside>
 </div>
-
-<!-- container -->
-
-<?php get_sidebar(); ?>
 
 <?php get_footer(); ?>
