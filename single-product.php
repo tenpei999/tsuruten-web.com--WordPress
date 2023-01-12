5a625c2594e0edaa5200a774954a7ff7a368640f
tenpei999 <tenpei999@gmail.com>
2023-01-12 20:35:16 +0900

    [feat]下層ページレイアウト統合

--- single-product.php
+++ single-product.php
@@ -1,26 +1,26 @@
 <?php get_header(); ?>
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-    <main class="l-main--custom-post">
+    <section class="p-main-visual" style="background-image: url(
+  <?php echo wp_get_attachment_url(get_post_thumbnail_id()); ?>)">
 
-      <section class="p-main-visual">
-
-        <div style="background-image: url(
-        <?php echo wp_get_attachment_url(get_post_thumbnail_id()); ?>)" class="p-main-visual">
-
-          <h2 class="p-main-visual__title">
-            <p class="p-main-visual__title-sub">
-              作品紹介
-            </p>
-            <p class="c-title--section p-main-visual__title-main">
-              <?php the_title(); ?>
-            </p>
-          </h2>
-        </div>
-      </section>
-
-      <?php the_content(); ?>
-    </main>
+      <h2 class="p-main-visual__title">
+        <p class="p-main-visual__title-sub">
+          作品紹介
+        </p>
+        <p class="c-title--section p-main-visual__title-main">
+          <?php the_title(); ?>
+        </p>
+      </h2>
+    </section>
+    <div class="l-wrapper p-wrapper">
+      <main class="l-main--products p-main--products">
+        <?php the_content(); ?>
+      </main>
+      <aside class="l-sidebar p-sidebar">
+        <?php get_sidebar(); ?>
+      </aside>
+    </div>
 
 
 <?php endwhile;
--- single.php
+++ single.php
@@ -1,26 +1,26 @@
 <?php get_header(); ?>
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-<section class="p-main-visual">
 
-  <div style="background-image: url(
-  <?php echo wp_get_attachment_url(get_post_thumbnail_id()); ?>)" class="p-main-visual">
+    <section class="p-main-visual" style="background-image: url(
+  <?php echo wp_get_attachment_url(get_post_thumbnail_id()); ?>)">
 
-    <h2 class="p-main-visual__title">
-      <p class="p-main-visual__title-sub">
-        Blog
-      </p>
-      <p class="c-title--section p-main-visual__title-main">
-        <?php the_title(); ?>
-      </p>
-    </h2>
-  </div>
-</section>
-
-<main class="l-main--post">
-
-  <?php the_content(); ?>
-  
-</main>
+      <h2 class="p-main-visual__title">
+        <p class="p-main-visual__title-sub">
+          Blog
+        </p>
+        <p class="c-title--section p-main-visual__title-main">
+          <?php the_title(); ?>
+        </p>
+      </h2>
+    </section>
+    <div class="l-wrapper p-wrapper">
+      <main class="l-main--products p-main--products">
+        <?php the_content(); ?>
+      </main>
+      <aside class="l-sidebar p-sidebar">
+        <?php get_sidebar(); ?>
+      </aside>
+    </div>
 
 
 <?php endwhile;
