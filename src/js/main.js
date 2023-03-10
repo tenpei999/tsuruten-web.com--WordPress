7be785f3f96edb4ad5e68c22678642e4bc75ff34
tenpei999 <tenpei999@gmail.com>
2023-03-10 23:33:18 +0900

    [feat]問題箇所修正中

--- src/js/main.js
+++ src/js/main.js
@@ -2,25 +2,25 @@
 // .p-page-topをクリックした際の設定
 const pageTop = document.querySelector('.p-page-top');
 
-pageTop.addEventListener('click', function() {
+pageTop.addEventListener('click', function () {
   const scroll = document.documentElement.scrollTop || document.body.scrollTop;
+  // const targets = document.querySelectorAll('body,html,.l-main');
   //スクロール値を取得
   if (scroll > 0) {
     pageTop.classList.add('floatAnime');
     //クリックしたらfloatAnimeというクラス名が付与
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth'
+    jQuery('body,html,.l-main').animate({ scrollTop: 0 }, 3000, 'swing', function () {
+      //スクロールの速さ。数字が大きくなるほど遅くなる
+      pageTop.classList.remove('floatAnime');
     });
     //スクロールの速さ。数字が大きくなるほど遅くなる
-    setTimeout(() => {
-      pageTop.classList.remove('floatAnime');
-    }, 3000);
   }
   return false;//リンク自体の無効化
 });
 
 // jQuery(function () {
+//   const pageTop = document.querySelector('.p-page-top');
+  
 //   jQuery(".p-page-top").on("click", function () {
 //     const scroll = document.documentElement.scrollTop || document.body.scrollTop;
 //     //スクロール値を取得
@@ -41,13 +41,15 @@ pageTop.addEventListener('click', function() {
 'scroll load'.split(' ').forEach((inokichiDown) => {
   window.addEventListener(inokichiDown, () => {
 
-    let winHeight = window.pageYOffset;
+    const winHeight = window.pageYOffset;
+    const pageTop = document.querySelector('.p-page-top');
+
     if (winHeight >= 100) {//上から100pxスクロールしたら
       pageTop.classList.add('UpMove');//#page-topについているUpMoveというクラス名を付与
       pageTop.classList.remove('DownMove');//#page-topについているDownMoveというクラス名を除く
     } else {
       //すでに#page-topにUpMoveというクラス名がついていたら
-      pageTop.classList.add('UpMove');//UpMoveというクラス名を除き
+      pageTop.classList.remove('UpMove');//UpMoveというクラス名を除き
       pageTop.classList.add('DownMove');//DownMoveというクラス名を#page-topに付与
     }
   })
